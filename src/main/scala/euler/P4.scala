1961d9be4b29706a74db4115ee815badf065db72
Josh Joyce <jjoyce@drwholdings.com>
2010-04-14 22:45:01 -0500

    prob. 4 done

---
+++ src/main/scala/euler/P4.scala
@@ -0,0 +1,16 @@
+package euler
+
+object P4 {
+  def isPalindromic(n: Int): Boolean = {
+    val s = n.toString
+    s == s.reverse.toString
+  }
+
+  def apply() {
+    val seq = for {i <- Stream.range(999, 99, -1)
+                   j <- Stream.range(999, 99, -1)} 
+                yield (i *j)
+    val x = seq filter { isPalindromic } reduceLeft {(a,b) => if (a > b) a else b}
+    println(x)
+  }
+}
