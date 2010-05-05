155225c2d4eebb54e0bf215e7404bd63fe31f4e2
Josh Joyce <jjoyce@drwholdings.com>
2010-05-04 21:05:32 -0500

    prob. 10 done

---
+++ src/main/scala/euler/P10.scala
@@ -0,0 +1,32 @@
+package euler
+
+import java.util.ArrayList
+import java.util.concurrent._
+
+class PrimesFinder(start: Int, stop: Int) extends Callable[List[Int]] {
+  import Math._
+
+  def isPrime(n: Int) = {
+    !(2 to ceil(sqrt(n)).asInstanceOf[Int]).exists(n % _ == 0)
+  }
+
+  def call() = Stream.range(start, stop).filter(isPrime).force
+}
+
+object P10 {
+  def apply() = {
+    val execSvc = Executors.newFixedThreadPool(2)
+    val callables = new ArrayList[Callable[List[Int]]]
+    callables.add(new PrimesFinder(2, 500000))
+    callables.add(new PrimesFinder(500001, 1000000))
+    callables.add(new PrimesFinder(1000001, 1500000))
+    callables.add(new PrimesFinder(1500001, 2000000))
+    val futures = execSvc.invokeAll(callables)
+    val it = futures.iterator()
+    var sum = BigInt(0)
+    while (it.hasNext()) {
+      sum += it.next().get().foldLeft(BigInt(0)) {_+_}
+    }
+    sum
+  }
+}
