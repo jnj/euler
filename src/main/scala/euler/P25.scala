d2dbf73bbdcd9248ace493b29b18ec82eb7c6a7f
Josh Joyce <jjoyce@drwholdings.com>
2011-01-11 14:39:37 -0600

    move Fibs into own namespace

--- src/main/scala/euler/P25.scala
+++ src/main/scala/euler/P25.scala
@@ -2,21 +2,21 @@ package euler
 
 import scala.math.BigInt
 
-class Fibs extends Iterator[BigInt] {
-  var a = BigInt(0)
-  var b = BigInt(1)
-  
-  override def hasNext = true
-  
-  override def next = {
-    val c = a
-    a = b
-    b += c
-    a
-  }
-}
-
 object P25 {
+  class Fibs extends Iterator[BigInt] {
+    var a = BigInt(0)
+    var b = BigInt(1)
+    
+    override def hasNext = true
+    
+    override def next = {
+      val c = a
+      a = b
+      b += c
+      a
+    }
+  }
+
   def apply() {
     val fibs = new Fibs
     fibs.indexWhere(_.toString.size >= 1000) match {
