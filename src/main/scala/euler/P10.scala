2f9af76081ad2bdcecc25c479f65087a45e73765
Josh Joyce <jnjoyce@speakeasy.net>
2011-01-23 21:01:55 -0600

    don't sum primes on main thread

--- src/main/scala/euler/P10.scala
+++ src/main/scala/euler/P10.scala
@@ -9,21 +9,20 @@ object P10 {
   implicit val pool = Executors.newFixedThreadPool(3)
   implicit val strategy = Strategy.Executor
 
-  def findPrimes(range: Traversable[Int]): Promise[List[Int]] = {
+  def sumPrimes(range: Traversable[Int]): Promise[BigInt] = {
     val isPrime = (n: Int) => {
       val lim = ceil(sqrt(n)).asInstanceOf[Int]
       !(2 to lim).exists(n % _ == 0)
     }
-    promise(range.filter(_ % 2 != 0).filter(isPrime).toList)
+    val isOdd = (i: Int) => i % 2 == 1
+    val zero = BigInt(0)
+    promise(range.filter(isOdd).filter(isPrime).foldLeft(zero) {_+_})
   }
 
   def apply() = {
     try {
-      val promises = (2 to 2000000).grouped(500000).map(findPrimes)
-      promises.foldLeft(BigInt(2)) {
-        val zero = BigInt(0)
-        (sum, primes) => sum + primes.get.foldLeft(zero) {_+_}
-      }
+      val promises = (2 to 2000000).grouped(500000).map(sumPrimes)
+      promises.foldLeft(BigInt(2)) {(s, p) => s + p.get}
     } finally {
       pool.shutdown()
     }
