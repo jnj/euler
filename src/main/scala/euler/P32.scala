ab9982b89aec19b85c48b4e8adb3b99fd570eb1f
Josh Joyce <jnjoyce@speakeasy.net>
2012-06-30 10:49:35 -0500

    p32

--- src/main/scala/euler/P32.scala
+++ src/main/scala/euler/P32.scala
@@ -7,15 +7,19 @@ object P32 {
   
   def apply() {
     val sums = for {
-      i <- (11 to 99)
-      j <- (101 to 999)
+      i <- (2 to 98)
+      j <- (123 to 9876)
       k = i * j
-      if k < 9999
       s = i.toString + j.toString + k.toString
       if s.size == 9
       if isPandigital(s.toInt)
-    } yield k
-    
-    println(sums.toSet.sum)
+    } yield (i, j)
+
+    sums.foreach {
+      case (i, j) => println("%s * %s = %s".format(i, j, i * j))
+    }
+
+    println(sums.map {case (i, j) => i*j}.toSet)
+    println(sums.map {case(i, j) => i*j}.toSet.sum)
   }
 }
